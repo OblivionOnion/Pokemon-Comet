26924bd308f6ec1dd615a2b67fc1381e25739dce
OblivionOnion <oblivionthomas@aol.com>
2015-08-06 09:15:24 -0400

    Create maintenance.php

---
+++ maintenance.php
@@ -0,0 +1,15 @@
+<head>
+<title>Maintenance</title>
+<style type='text/css'>
+body {
+color: #FFFFFF;
+background-color: #000000;
+}
+</style>
+</head>
+<div style='text-align: center;'><h1>Down For Mainteance</h1><br>
+<br><u>Reasons:</u><br>
+- Resetting. <br><i>Due to many people suggestioning a reset, and I also think it would be quite an idea, thats what I am doing. Also, I'm updating Register so that you can choose to show/hide your email and choose your starter. Aswell, Thanks to HuMaJuMa, we are going to have a new color scheme, I think everyone will love it.</i>
+<br><br><strong>ChatBox:</strong><br>
+<embed src="http://www.xatech.com/web_gear/chat/chat.swf" quality="high" width="540" height="405" name="chat" FlashVars="id=88781801" align="middle" allowScriptAccess="sameDomain" type="application/x-shockwave-flash" pluginspage="http://xat.com/update_flash.shtml" />
+</div>
