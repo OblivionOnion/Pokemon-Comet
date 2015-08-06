e5f08b37a7b459868b0b42d7ca6398feb33fb836
OblivionOnion <oblivionthomas@aol.com>
2015-08-06 09:18:56 -0400

    Create test2.php

---
+++ test2.php
@@ -0,0 +1,13 @@
+<style type='text/css'>
+#map {
+width: 400px;
+height: 400px;
+background: url('http://i46.tinypic.com/2zs46te.png');
+}
+#char {
+position: relative;
+top: 11px;
+left: 18px;
+}
+</style>
+<div id='map'><img src='http://i45.tinypic.com/35wes.png' title='Char' alt='X' id='char'></div>
